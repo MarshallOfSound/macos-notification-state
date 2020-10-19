6c0997056724b98a13cb76ab783eccc6c9f3c71d
Samuel Attard <samuel.r.attard@gmail.com>
2020-10-19 16:22:21 -0700

    refactor: split out big sur logic into helper methods for safety

--- lib/do-not-disturb.mm
+++ lib/do-not-disturb.mm
@@ -1,13 +1,24 @@
 #import <Foundation/Foundation.h>
 
-bool getDoNotDisturb() {
-  bool doNotDisturb;
-  NSOperatingSystemVersion version = [[NSProcessInfo processInfo] operatingSystemVersion];
-  bool isBigSur = version.majorVersion == 11 || (version.majorVersion == 10 && version.minorVersion > 15);
-  if (!isBigSur) {
-    doNotDisturb = [[[[NSUserDefaults alloc] initWithSuiteName:@"com.apple.notificationcenterui"] objectForKey:@"doNotDisturb"] boolValue];
-  } else {
-    doNotDisturb = [[[[NSUserDefaults alloc] initWithSuiteName:@"com.apple.controlcenter"] objectForKey:@"NSStatusItem Visible DoNotDisturb"] boolValue];
-  }
+static NSOperatingSystemVersion version = [[NSProcessInfo processInfo] operatingSystemVersion];
+static bool isBigSur = version.majorVersion == 11 || (version.majorVersion == 10 && version.minorVersion > 15);
+
+bool getDoNotDisturbBigSur() {
+  bool doNotDisturb = [[[[NSUserDefaults alloc] initWithSuiteName:@"com.apple.notificationcenterui"] objectForKey:@"doNotDisturb"] boolValue];
+  NSCAssert(isBigSur, @"must be on big sur");
   return doNotDisturb;
+}
+
+bool getDoNotDisturbOlderThanBigSur() {
+  bool doNotDisturb = doNotDisturb = [[[[NSUserDefaults alloc] initWithSuiteName:@"com.apple.controlcenter"] objectForKey:@"NSStatusItem Visible DoNotDisturb"] boolValue];
+  NSCAssert(isBigSur, @"must not be on big sur");
+  return doNotDisturb;
+}
+
+bool getDoNotDisturb() {
+  if (!isBigSur) {
+    return getDoNotDisturbOlderThanBigSur();
+  } else {
+    return getDoNotDisturbBigSur();
+  }
 }
